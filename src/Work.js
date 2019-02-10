3d6db8ae5e072e71692c43b5a7919b0e9552a775
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2019-02-09 16:18:24 -0800

    finished implementing the finger food work page, without links and pushstack calls from within the modal, animations are also still missing and dynamic scss needs to be implemented as well

--- src/Work.js
+++ src/Work.js
@@ -5,6 +5,9 @@ import About from './About';
 import './styles/WorkOne.scss';
 import './styles/Navigation.scss';
 import ffLogo from './img/ff_logo_red_.png';
+import krogerLogo from './img/Current_Kroger_logo.png';
+import spheroLogo from './img/sphero2x.png';
+import luluLogo from './img/lulu@2x.png';
 
 class Work extends Component {
   constructor(props){
@@ -31,6 +34,11 @@ class Work extends Component {
         <div className="the-job-header"><span>QA Lead &#38; Analytics developer</span></div>
         <div className="ff-job-details-button"><div className="details-btn-text">Details...</div></div>
         <div className="projects-header"><span>Disclosable Projects:</span></div>
+        <div className="projects-container">
+        <img className="lulu-logo" src={luluLogo} alt="Lululemon"></img>
+        <img className="kroger-logo" src={krogerLogo} alt="Kroger"></img>
+        <img className="sphero-logo" src={spheroLogo} alt="sphero"></img>
+        </div>
         <div onClick={() => this.state.NavigationViewer.pushToNavigationStack(About)}><div className= "more-button"></div></div>
         <div onClick={() => this.state.NavigationViewer.popFromNavigationStack()}><div className= "back-button"></div></div>
         <div onClick={() => this.state.NavigationViewer.pushToNavigationStack(About)}><div className= "next-button"></div></div>
