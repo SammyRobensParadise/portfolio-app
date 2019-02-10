ca209da8af84556dd1924c4b9d19fac28ba3bea3
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2019-02-09 18:44:55 -0800

    continued implementing first about page

--- src/About.js
+++ src/About.js
@@ -3,6 +3,9 @@ import NavigationHandler from './NavigationHandler';
 import './styles/AboutOne.scss';
 import './styles/Navigation.scss';
 import AboutTwo from './AboutTwo';
+import WaterlooTxt from './assets/waterloo_txt.svg';
+import VancouverTxt from './assets/vancouver_txt.svg';
+
 import anime from 'animejs';
 
 class About extends Component {
@@ -22,6 +25,15 @@ class About extends Component {
     //ANIMATIONS REQUIRED
     return (
       <div className="About-one">
+      <div className="aboutone-title">About</div>
+      <div className="background-parent">
+      <div className="west-coast-background"></div>
+      <div className="vertical-text">
+      <div className="waterloo-text"><p>Waterloo Ontario</p></div>
+      <div className="vancouver-text">Vancouver BC</div>
+      </div>
+      </div>
+
         <div onClick={() => this.state.NavigationViewer.popFromNavigationStack()}><div className= "back-button"></div></div>
         <div onClick={() => this.state.NavigationViewer.pushToNavigationStack(AboutTwo)}><div className= "next-button"></div></div>
       </div>
