670e32a544fef0c4ce92fbd726f2b6809d273fc8
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2019-03-28 22:11:34 -0700

    ammended css and jsx tohave responsive columns

--- src/AboutTwo.js
+++ src/AboutTwo.js
@@ -54,25 +54,34 @@ class AboutTwo extends Component {
     //ANIMATIONS REQUIRED
     return (
       <div className="About-two">
-      <div className="navigation-bar">
-      <ul>
-      <li className="home" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Home)}><span className="navigation">Start Over...</span></li>
-       </ul>
-       </div>
-    <div className="abouttwo-title">Education</div>
-      <div className="background-parent-about-two">
-      <div className="waterloo-background"></div>
-      <div className="the-about-systems-header"><span>Systems Design Engineering, The university of Waterloo</span></div>
-      <div className="the-description-text-education-one"><p>As someone who advocates optimized engineering solutions,
-        I continue on my journey to find the most eloquent and beautiful solutions to the worlds most challening problems.
-        I have always taken an interest in the way humans interact with technology, and more specifically,
-        the way it shapes our experiences. I chose to study Systems Design Engineering because of my
-        fascination with the way one user's experience of a product or service can shape their understanding of the world,
+        <div className="navigation-bar">
+          <ul>
+            <li className="home" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Home)}><span className="navigation">Start Over...</span></li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-header">
+            <div className="about-two-title">Education</div>
+          </div>
+        </div>
+        <div className="row">
+        <div className="col-edu-main">
+        <div className="background-parent-about-two">
+          <div className="waterloo-background"></div>
+          <div className="the-about-systems-header"><span>Systems Design Engineering, The university of Waterloo</span></div>
+          <div className="the-description-text-education-one"><p>As someone who advocates optimized engineering solutions,
+            I continue on my journey to find the most eloquent and beautiful solutions to the worlds most challening problems.
+            I have always taken an interest in the way humans interact with technology, and more specifically,
+            the way it shapes our experiences. I chose to study Systems Design Engineering because of my
+            fascination with the way one user's experience of a product or service can shape their understanding of the world,
         and more specifically, the unique impact of that understanding.</p></div>
-        <div className="waterloo-grad-year"><span>2017-2023</span></div>
-      </div>
-        <div onClick={() => this.state.NavigationViewer.popFromNavigationStack()}><div className= "back-button"></div></div>
-        <div onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.AboutThree)}><div className= "next-button"><div className="glow-button-inner-horizontal"></div></div></div>
+          <div className="waterloo-grad-year"><span>2017-2023</span></div>
+        </div>
+        </div>
+        </div>
+
+        <div onClick={() => this.state.NavigationViewer.popFromNavigationStack()}><div className="back-button"></div></div>
+        <div onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.AboutThree)}><div className="next-button"><div className="glow-button-inner-horizontal"></div></div></div>
         <div className="screen-number"><p>2/3</p></div>
       </div>
     );
