9fbbbdc7e5a2b67c6e8a1ea85bdf2308223a223e
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-04-26 12:17:07 -0500

    updated readme.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 
+# Written By Sammy Robens-Paradise, Systems Design Engineering, The University of Waterloo.
+
+This is Sammy's personal portfolio, hosted on google firebase. It is built in using react.js
+I am a Developer, UX Designer, Artist and Student at the University of Waterloo.
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -10,6 +15,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.<br>
 You will also see any lint errors in the console.
+This may be used for development only.
 
 ### `npm test`
 
@@ -18,4 +24,22 @@ See the section about [running tests](https://facebook.github.io/create-react-ap
 
 ### `npm run build`
 
+This will create a production build.<br>
 
+
+## Depencies
+
+`   "animejs": "^3.0.1",
+    "classnames": "^2.2.6",
+    "node-sass": "^4.11.0",
+    "react": "^16.7.0",
+    "react-cookie": "^3.0.8",
+    "react-dom": "^16.7.0",
+    "react-facebook": "^6.0.15",
+    "react-router-dom": "^4.3.1",
+    "react-scripts": "2.1.3",
+    "react-transition-group": "^1.2.1",
+`
+### Contact
+
+srobensparadise@gmail.com
--- src/sub_components/Socials.js
+++ src/sub_components/Socials.js
@@ -9,7 +9,7 @@ import anime from 'animejs';
 //import Resume from '../Resume';
 
 class Socials extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       isRendered: false,
@@ -18,98 +18,98 @@ class Socials extends Component {
       currentClickCount: 0,
       NavigationViewer: new NavigationHandler()
     }
-    }
-    componentDidMount(){
-      setTimeout( () => {
-        this.setState({isRendered: true});
-      },500);
-    }
-    componentDidUpdate(){
-      let PrevClickCount = this.props.currentClickCount;
-      if(PrevClickCount > this.state.currentClickCount){
-        this.ShareContract();
-        this.ContactContract();
-        setTimeout(() => {
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isRendered: true });
+    }, 500);
+  }
+  componentDidUpdate() {
+    let PrevClickCount = this.props.currentClickCount;
+    if (PrevClickCount > this.state.currentClickCount) {
+      this.ShareContract();
+      this.ContactContract();
+      setTimeout(() => {
         this.setState({
           showContactExpanded: false,
           showShareExpanded: false
         });
-      },600);
-        this.setState({currentClickCount: PrevClickCount});
-      }  
+      }, 600);
+      this.setState({ currentClickCount: PrevClickCount });
     }
-    toggledShare = () => {
-      this.setState({showContactExpanded: false});
-      if(!this.state.showShareExpanded){
-      this.setState({showShareExpanded: true});
-      setTimeout(() => {this.ShareExpand()},1)
-      } else {
-       // this.setState({showShareExpanded: false});
-      }
+  }
+  toggledShare = () => {
+    this.setState({ showContactExpanded: false });
+    if (!this.state.showShareExpanded) {
+      this.setState({ showShareExpanded: true });
+      setTimeout(() => { this.ShareExpand() }, 1)
+    } else {
+      // this.setState({showShareExpanded: false});
     }
-    toggleContact = () => {
-      this.setState({showShareExpanded: false});
-      if(!this.state.showContactExpanded){
-      this.setState({showContactExpanded: true});
-      setTimeout(() => {this.ContactExpand()},1)
-      } else {
-        // this.setState({showContactExpanded: false});
-      }
+  }
+  toggleContact = () => {
+    this.setState({ showShareExpanded: false });
+    if (!this.state.showContactExpanded) {
+      this.setState({ showContactExpanded: true });
+      setTimeout(() => { this.ContactExpand() }, 1)
+    } else {
+      // this.setState({showContactExpanded: false});
     }
+  }
 
-    //expand the share buttons pannel ---------------------------------
-    ShareExpand = () =>{
-      anime({
-        targets: '.icon-fb',
-        translateX: '-6rem',
-        opacity: [0,1],
-        duration: 600,
-      })
-      anime({
-        targets: '.icon-instagram',
-        translateX: '-6rem',
-        opacity: [0,1],
-        delay: 100,
-        duration: 600,
-      })
-    }
-    //close the share buttons pannel ----------------------------------
-    ShareContract = () => {
-      anime({
-        targets: '.icon-fb',
-        translateX: '6rem',
-        opacity: [1,0],
-        delay: 200,
-        duration: 600,
-      })
-      anime({
-        targets: '.icon-instagram',
-        translateX: '6rem',
-        opacity: [1,0],
-        delay: 100,
-        duration: 600,
-      })
-    }
+  //expand the share buttons pannel ---------------------------------
+  ShareExpand = () => {
+    anime({
+      targets: '.icon-fb',
+      translateX: '-6rem',
+      opacity: [0, 1],
+      duration: 600,
+    })
+    anime({
+      targets: '.icon-instagram',
+      translateX: '-6rem',
+      opacity: [0, 1],
+      delay: 100,
+      duration: 600,
+    })
+  }
+  //close the share buttons pannel ----------------------------------
+  ShareContract = () => {
+    anime({
+      targets: '.icon-fb',
+      translateX: '6rem',
+      opacity: [1, 0],
+      delay: 200,
+      duration: 600,
+    })
+    anime({
+      targets: '.icon-instagram',
+      translateX: '6rem',
+      opacity: [1, 0],
+      delay: 100,
+      duration: 600,
+    })
+  }
   //expand Contact pannel ---------------------------------------
-  ContactExpand = () =>{
+  ContactExpand = () => {
     anime({
       targets: '.icon-github',
       translateX: '-6rem',
-      opacity: [0,1],
+      opacity: [0, 1],
       duration: 600,
       delay: 100
     })
     anime({
       targets: '.icon-email',
       translateX: '-6rem',
-      opacity: [0,1],
+      opacity: [0, 1],
       delay: 200,
       duration: 600,
     })
     anime({
       targets: '.icon-linkedin',
       translateX: '-6rem',
-      opacity: [0,1],
+      opacity: [0, 1],
       delay: 300,
       duration: 600,
     })
@@ -118,76 +118,76 @@ class Socials extends Component {
     anime({
       targets: '.icon-github',
       translateX: '6rem',
-      opacity: [1,0],
+      opacity: [1, 0],
       duration: 600,
       delay: 200
     })
     anime({
       targets: '.icon-email',
       translateX: '6rem',
-      opacity: [1,0],
+      opacity: [1, 0],
       duration: 600,
       delay: 100
     })
     anime({
       targets: '.icon-linkedin',
       translateX: '6rem',
-      opacity: [1,0],
+      opacity: [1, 0],
       duration: 600,
     })
   }
 
   render() {
-  const shareList = <div className="expanded-share">
-  <div className="icon-fb-outer">
-   <FacebookProvider appId="813508179041617">
- <ShareButton className="icon-fb" href="https://portfolio-app-1091c.firebaseapp.com/">
-  </ShareButton>
- </FacebookProvider>
- </div>
- <a id="instagram-link" href="https://www.instagram.com/stereo_west/" target="_blank" rel="noopener noreferrer"><div className="icon-instagram"></div></a></div>
+    const shareList = <div className="expanded-share">
+      <div className="icon-fb-outer">
+        <FacebookProvider appId="813508179041617">
+          <ShareButton className="icon-fb" href="https://portfolio-app-1091c.firebaseapp.com/">
+          </ShareButton>
+        </FacebookProvider>
+      </div>
+      <a id="instagram-link" href="https://www.instagram.com/stereo_west/" target="_blank" rel="noopener noreferrer"><div className="icon-instagram"></div></a></div>
 
-  const contactList = <div className="expanded-contact"><a id="github-link" href="https://github.com/SammyRobensParadise" target="_blank" rel="noopener noreferrer"><div className="icon-github"></div></a>
-    <a href="mailto:srobensp@edu.uwaterloo.ca?Subject=Inquiry%20From%20Website" target="_top"><div className="icon-email"></div></a>
-    <a id="linkedin-link" href="https://www.linkedin.com/in/sammy-robens-paradise/" target="_blank" rel="noopener noreferrer"><div className="icon-linkedin"></div></a>
+    const contactList = <div className="expanded-contact"><a id="github-link" href="https://github.com/SammyRobensParadise" target="_blank" rel="noopener noreferrer"><div className="icon-github"></div></a>
+      <a href="mailto:srobensp@edu.uwaterloo.ca?Subject=Inquiry%20From%20Website" target="_top"><div className="icon-email"></div></a>
+      <a id="linkedin-link" href="https://www.linkedin.com/in/sammy-robens-paradise/" target="_blank" rel="noopener noreferrer"><div className="icon-linkedin"></div></a>
     </div>
     let showContact = this.state.showContactExpanded;
     let showShare = this.state.showShareExpanded;
 
-    if(!showShare && !showContact){
-    return (
-      <div className="social">
-      <div className="btn-contain">
-      <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
-      <div className="btn"><div className="icon-social" onClick={this.toggledShare}></div></div>
-      <div className="btn"><div className="icon-contact" onClick={this.toggleContact}></div></div>
-      </div>
-      </div>
-    );
-    } else
-    if(showShare & !showContact){
+    if (!showShare && !showContact) {
       return (
-      <div className="social">
-      <div className="btn-contain">
-      <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
-      <div className="btn"><span>{shareList}<div className="icon-social" onClick={this.toggledShare}></div></span></div>
-      <div className="btn"><div className="icon-contact" onClick={this.toggleContact}></div></div>
-      </div>
-      </div>
+        <div className="social">
+          <div className="btn-contain">
+            <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
+            <div className="btn"><div className="icon-social" onClick={this.toggledShare}></div></div>
+            <div className="btn"><div className="icon-contact" onClick={this.toggleContact}></div></div>
+          </div>
+        </div>
       );
     } else
-    if(!showShare & showContact){
-      return (
-      <div className="social">
-      <div className="btn-contain">
-      <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
-      <div className="btn"><div className="icon-social" onClick={this.toggledShare}></div></div>
-      <div className="btn"><span>{contactList}<div className="icon-contact" onClick={this.toggleContact}></div></span></div>
-      </div>
-      </div>
-      );
-    }
+      if (showShare & !showContact) {
+        return (
+          <div className="social">
+            <div className="btn-contain">
+              <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
+              <div className="btn"><span>{shareList}<div className="icon-social" onClick={this.toggledShare}></div></span></div>
+              <div className="btn"><div className="icon-contact" onClick={this.toggleContact}></div></div>
+            </div>
+          </div>
+        );
+      } else
+        if (!showShare & showContact) {
+          return (
+            <div className="social">
+              <div className="btn-contain">
+                <div className="btn"><div className="icon-resume" onClick={() => this.state.NavigationViewer.pushToNavigationStack(ScreenEnum.Resume)}></div></div>
+                <div className="btn"><div className="icon-social" onClick={this.toggledShare}></div></div>
+                <div className="btn"><span>{contactList}<div className="icon-contact" onClick={this.toggleContact}></div></span></div>
+              </div>
+            </div>
+          );
+        }
 
-}
+  }
 }
 export default Socials;
